f01fb6aa170ea108003ed03c12c6b8ff55d6e53f
AashikaLilaramani <84186621+AashikaLilaramani@users.noreply.github.com>
2021-09-03 12:52:28 -0500

    did instru + first button

--- app/src/main/java/com/example/madlibsapp/MainActivity.java
+++ app/src/main/java/com/example/madlibsapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.madlibsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void newScreen(View v)
+    {
+        Intent intent  = new Intent(this, enterWords.class);
+        startActivity(intent);
+    }
+    public void instructions(View v)
+    {
+        String theIntro = "Once you click the continue button, you will be taken " +
+                "to a screen where you can enter in words as directed. " +
+                "Once you are done with that, you will see a story with " +
+                "the words you inputted.";
+        TextView intro = findViewById(R.id.introTextView);
+        intro.setText(theIntro);
+        intro.setVisibility(View.VISIBLE);
+
     }
 }
